1ba6405f9c1fca2db708762b9c9f3de85a4cf3ea
Toyehamzat <anibiseun@gmail.com>
2024-01-10 14:19:15 +0000

    demo completed

---
+++ app/(protect)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,10 @@
+import { OrganizationList, UserButton } from "@clerk/nextjs";
+import React from "react";
+
+export default function CreateOrganizationPage() {
+  return (
+    <div>
+      <OrganizationList />
+    </div>
+  );
+}
